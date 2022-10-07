d9c5141e4294c8ec3ca881734c1d620dbeae1f02
avramies <aschwalb@mail.yu.edu>
2022-10-07 16:05:55 -0400

    Update seleniumR.R

--- project/seleniumR.R
+++ project/seleniumR.R
@@ -10,7 +10,7 @@ library(data.table)
 remDr <- rsDriver(browser = "firefox",
                   port=4445L)
 
-clean <- function(){
+clean_nba <- function(){
   season = "2011-2012"
   pages = 1:23
   allData = NA
@@ -35,8 +35,33 @@ clean <- function(){
   }
   write.csv(allData, file = paste0("/Users/avrahamschwalb/Documents/value_sports_betting/project/data/nba/",season,".csv"), row.names = F)
 }
-debugonce(clean)
-cleaned = clean()
+debugonce(clean_nba)
+cleaned = clean_nba()
 
 
+clean_mlb <- function(pages,season){
 
+  allData = NA
+  for(page in pages){
+    remDr$client$navigate(paste0("https://www.oddsportal.com/baseball/usa/mlb-",season,"/results/#/page/",page,"/"))
+    mlb <- remDr$client$findElement(using = "id", "tournamentTable")
+    mlb_html <- mlb$getPageSource()
+    the_full_html <- read_html(mlb_html %>% unlist())
+    data_frame = html_table(the_full_html)
+    colnames(data_frame[[1]]) <- c(1,2,3,4,5,6,7)
+    data_frame[[1]]$'5' = na_if(data_frame[[1]]$'5', "-")
+    data_frame[[1]]$'6' = na_if(data_frame[[1]]$'6', "-")
+    
+    cleaned_dataframe = clean_scraped(as.data.frame(data_frame[[1]]))
+    
+    if(!is.data.frame(allData)){
+      allData = cleaned_dataframe
+    }else{
+      allData = rbind(allData, cleaned_dataframe)
+    }
+    
+  }
+  write.csv(allData, file = paste0("/Users/avrahamschwalb/Documents/value_sports_betting/project/data/mlb/",season,".csv"), row.names = F)
+}
+
+clean_mlb(1:58, "2021")
