c26597c9e23dbf5d3ce03dccb0f56def975d2a42
avramies <aschwalb@mail.yu.edu>
2022-10-06 22:15:36 -0400

    add betting package

---
+++ betting/R/convert_odds.R
@@ -0,0 +1,14 @@
+# function for converting odds
+convert_odds <- function(odds, initial_odds_type = "american"){
+  if(initial_odds_type == "american"){
+    if(odds > 0){
+      decimal = 1 + (odds / 100)
+    }else{
+      decimal = 1 - (100 / odds)
+    }
+    return(decimal)
+  }else if (initial_odds_type == "fractional"){
+    decimal = odds + 1
+    return (decimal)
+  }
+}
---
+++ betting/R/odds_to_implied_prob.R
@@ -0,0 +1,15 @@
+# function for converting decimal odds to implied probabilities
+odds_to_implied_probability <- function(odds, odds_type = "decimal"){
+  if (odds_type == "fractional"){
+    odds = convert_odds(odds, initial_odds_type = "fractional")
+    prob = 1 / odds
+    return(prob)
+  } else if (odds_type == "american"){
+    odds = convert_odds(odds, initial_odds_type = "american")
+    prob = 1 / odds
+    return(prob)
+  }else if(odds_type == "decimal"){
+    prob = 1 / odds
+    return(prob)
+  }
+}
