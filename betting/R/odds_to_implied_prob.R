78c4cd4b3e318712ef697f51fddef24d4ae44596
avramies <aschwalb@mail.yu.edu>
2022-10-12 20:56:50 -0400

    Update odds_to_implied_prob.R

--- betting/R/odds_to_implied_prob.R
+++ betting/R/odds_to_implied_prob.R
@@ -1,7 +1,7 @@
 # function for converting decimal odds to implied probabilities
 odds_to_implied_probability <- function(odds){
   odds = as.numeric(odds)
-  odds = convert_odds(odds)
+  odds = convert_american_to_decimal(odds)
   prob = 1 / odds
   return(prob)
 }
