f1b77e4e1b82cf2bfb4d625a65fc8165751b303d
avramies <aschwalb@mail.yu.edu>
2022-10-12 21:04:35 -0400

    Update convert_american_to_decimal.R

--- betting/R/convert_american_to_decimal.R
+++ betting/R/convert_american_to_decimal.R
@@ -1,8 +1,8 @@
 # function for converting odds
 convert_american_to_decimal <- function(odds){
   odds = as.numeric(odds)
-    ifelse(odds > 0,
-           decimal = 1 + (odds / 100),
-           decimal = 1 - (100 / odds))
+    decimal = ifelse(odds > 0,
+                     1 + (odds / 100),
+                     1 - (100 / odds))
     return(decimal)
 }
